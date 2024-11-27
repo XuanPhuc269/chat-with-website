98284bf0e46f9f9fc9508056860a1d4713d56cfe
Xuan Phuc Tran <xuanphuctran269@gmail.com>
2024-11-28 00:35:57 +0700

    First Commit

---
+++ src/app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+
+def get_response(user_input):
+    return "I don't know"
+
+# App config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a bot. How can I help you?"),
+    ]
+
+
+# Sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL.")
+
+else:
+    # User input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+    # Conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)    
+
+
+
+
